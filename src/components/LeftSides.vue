c468f6fd7f1674bd3d0df2c8240dd718f620baa3
rdzfv <xuyuyan999@126.com>
2024-07-10 19:03:42 +0800

    fix: 解决通过地址栏访问的路由问题

--- src/components/LeftSides.vue
+++ src/components/LeftSides.vue
@@ -46,18 +46,25 @@ const get_directory_detail = (url) => {
 const emit = defineEmits(['choose_menu'])
 const choose_menu = (index, show_name, url, type) => {
   // todo 对url进行整形
+  let [protocal, relative_url] = url.split('://')
+  let reformed_url = relative_url
+  if (relative_url.indexOf("../") !== -1) {
+    reformed_url = relative_url.split('../')[0].split('/').slice(0, -2).join('/') + '/'
+  }
+  reformed_url = protocal + '://' + reformed_url
 
   // 文件情况
   if (type === 'file') {
     document.title = show_name
-    if (url.endsWith('/')) url = url.slice(0, -1)  // 历史遗留问题，确保文件结尾没有/
-    let directory_url = url.slice(0, url.lastIndexOf("/") + 1)  // 目录的结尾必须是/
+    if (reformed_url.endsWith('/')) reformed_url = reformed_url.slice(0, -1)  // 历史遗留问题，确保文件结尾没有/
+    formState.root_link = reformed_url
+    let directory_url = reformed_url.slice(0, reformed_url.lastIndexOf("/") + 1)  // 目录的结尾必须是/
     if (file_list.value.length === 0) {  // 从地址栏进入时播放列表未知，补充获取播放列表
       get_directory_detail(directory_url)
     }
 
     const choose_menu_result = {   // 传递参数给父组件，用于处理自动播放
-      "video_url": url,
+      "video_url": reformed_url,
       "play_list": file_list,
       "video_index": index
     }
@@ -66,8 +73,8 @@ const choose_menu = (index, show_name, url, type) => {
   }
 
   // 目录情况
-  formState.root_link = url
-  get_directory_detail(url)
+  formState.root_link = reformed_url
+  get_directory_detail(reformed_url)
 };
 
 
@@ -128,7 +135,18 @@ onMounted(() => {
 </template>
 
 <style scoped>
+#components-layout-demo-side .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
+}
 
+.site-layout .site-layout-background {
+  background: #fff;
+}
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
 </style>
 
 <script>
