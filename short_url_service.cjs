b5a14813f55644b8a96ee38dc07d4bf47bbb65d7
胡皓睿 <i@huhaorui.com>
2024-07-16 16:06:31 +0800

    feat: update generateShortUrl

--- short_url_service.cjs
+++ short_url_service.cjs
@@ -27,16 +27,15 @@ function getConnection() {
 
 // 生成短链接的函数
 async function generateShortUrl(longUrl) {
-    let hash;
     let shortUrl;
     // 检查冲突
-    let flag = true
     do {
-        hash = crypto.createHash('sha256').update(longUrl + Math.random().toString()).digest();
-        shortUrl = base64url.encode(hash.slice(0, 6));
+        shortUrl = Math.round((Math.random() * 0xFFFFFFFF)).toString(16).toUpperCase() //不如直接随机数吧，反正加不加url，其实没啥区别
+        while (shortUrl.length < 8) {
+            shortUrl = `0${shortUrl}`
+        }
         // 读数据库，检查短链接是否已经存在
-        flag = await getShortUrl(shortUrl)
-    } while (flag);
+    } while (await checkShortUrlExists(shortUrl));
     // 存储shortUrl
     await storeUrlMapping(longUrl, shortUrl);
     return shortUrl
@@ -62,7 +61,7 @@ async function getLongUrl(shortUrl) {
 }
 
 // 是否存在指定shortUrl
-async function getShortUrl(shortUrl) {
+async function checkShortUrlExists(shortUrl) {
     const query = 'SELECT short_url FROM url_mapping WHERE short_url = ?';
     const [results, fields] = await getConnection().execute(query, [shortUrl])
     return results.length > 0
