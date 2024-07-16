0ca31541647e9e88842707c45d7c394433f2c772
huhaorui <i@huhaorui.com>
2024-07-16 12:40:23 +0800

    fix: update hook.cjs

--- hook.cjs
+++ hook.cjs
@@ -5,17 +5,6 @@ const {exec} = require('child_process');
 
 app.use(express.json())
 
-ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-    console.log(`子进程退出码：${code}`);
-});
 
 
 app.post("/", (req, res) => {
