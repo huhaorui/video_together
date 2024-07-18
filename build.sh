3cbed90832174b479f594c21d72671c13115e103
huhaorui <i@huhaorui.com>
2024-07-18 13:42:41 +0800

    fix: update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
+source ~/.zshrc
 cd /www/wwwroot/together.huhaorui.com/video_together/
 git checkout master
 git pull
@@ -6,7 +7,7 @@ nvm use 20
 npm install
 npm run build
 rm *.zip
-zip -r files node_modules dist server.cjs
+zip -r files node_modules dist server.cjs short_url_service.cjs
 docker stop `docker ps -a | grep video_together| cut -c 1-12`
 docker build -t 'video_together' .
 docker run -p 7799:7799 video_together --restart=always &
